1720758f03a0480459abaea3bf20aba4c698818e
AyaOsaama <ayaosama40a@gmail.com>
2025-05-03 20:22:55 +0300

    edit cors

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ mongoose
     console.error("Failed to connect to the database:", err);
   });
 //middleware
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
